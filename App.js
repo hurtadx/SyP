0fe8e2b3b2896b91f4eafa5d1814c0617daa9562
Hurtadx <andreshm253@gmail.com>
2025-04-07 12:13:06 -0500

    Refactor App component to use bottom tab navigation and remove unused styles

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+
+import HomeScreen from './src/screens/Home';
+import MapScreen from './src/screens/Map';
+import MusicScreen from './src/screens/Music';
+import RouletteScreen from './src/screens/Roulette';
+import GalleryScreen from './src/screens/Gallery';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+            if (route.name === 'Inicio') {
+              iconName = focused ? 'heart' : 'heart-outline';
+            } else if (route.name === 'Mapa') {
+              iconName = focused ? 'map' : 'map-outline';
+            } else if (route.name === 'Música') {
+              iconName = focused ? 'musical-notes' : 'musical-notes-outline';
+            } else if (route.name === 'Ruleta') {
+              iconName = focused ? 'refresh-circle' : 'refresh-circle-outline';
+            } else if (route.name === 'Fotos') {
+              iconName = focused ? 'images' : 'images-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#ff6b6b',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Inicio" component={HomeScreen} />
+        <Tab.Screen name="Mapa" component={MapScreen} />
+        <Tab.Screen name="Música" component={MusicScreen} />
+        <Tab.Screen name="Ruleta" component={RouletteScreen} />
+        <Tab.Screen name="Fotos" component={GalleryScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
